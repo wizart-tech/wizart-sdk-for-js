a6a264f988a8ac468f4b93f48aa715db4f0e1b54
yauheni.dzirvuk <yauheni.dzirvuk@wizart.ai>
2022-07-15 17:50:46 +0300

    updated configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,44 @@
-var webpack        = require('webpack')
-  , path           = require('path')
-  , UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
-  , env            = process.env.WEBPACK_ENV
-  ;
+var webpack = require('webpack')
+    , path = require('path')
+    , env = process.env.WEBPACK_ENV
+;
 
 var libraryName = 'wizart-vision'
-  , outputFile  = ''
-  , plugins     = []
-  ;
+    , outputFile = ''
+    , plugins = []
+;
 
 // configure output for proper build type
 if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = libraryName + '.min.js';
+    outputFile = libraryName + '.min.js';
 } else {
-  outputFile = libraryName + '.js';
+    outputFile = libraryName + '.js';
 }
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  target: 'web',
-  mode: 'development',
-  entry: {
-    main: path.resolve(__dirname, './src/index.js')
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      }
-    ]
-  }
+    target: 'web',
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
 }
